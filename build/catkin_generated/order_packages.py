bad28b68492e0199953c1228045d2f53a2bb772e
karthik nambiar <karthiknambiar4u@gmail.com>
2023-05-31 04:49:28 +0000

    init

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/karthik/openvins/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/karthik/workspace/catkin_ws_ov/devel;/opt/ros/noetic'.split(';') if '/home/karthik/workspace/catkin_ws_ov/devel;/opt/ros/noetic' != '' else []
